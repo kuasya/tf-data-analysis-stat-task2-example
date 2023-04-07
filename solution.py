756e9d3c3bf68f4a19d85a4083947adc8fe44bfe
kuasya <62285192+kuasya@users.noreply.github.com>
2023-04-07 22:35:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,9 +7,21 @@ from scipy.stats import norm
 chat_id = 1031228237 # Ваш chat ID, не меняйте название переменной
 
 
+# def solution(p: float, x: np.array) -> tuple:
+#     alpha = 1 - p
+#     loc = x.mean()
+#     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+#     return loc - scale * norm.ppf(1 - alpha / 2), \
+#            loc - scale * norm.ppf(alpha / 2)
+
 def solution(p: float, x: np.array) -> tuple:
+    n = len(x)
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    mean = x.mean()
+    std = np.sqrt(np.var(x))
+    dof = n - 1
+    t_value = abs(t.ppf(alpha/2, dof))
+    conf_int = t_value * std / np.sqrt(n)
+    lower = mean - conf_int
+    upper = mean + conf_int
+    return (lower, upper)
