ec7b263c56fcb7b5e7506d031d777d0bd39bec10
kuasya <62285192+kuasya@users.noreply.github.com>
2023-04-10 09:40:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,13 +14,20 @@ chat_id = 1031228237 # Ваш chat ID, не меняйте название пе
 #     return loc - scale * norm.ppf(1 - alpha / 2), \
 #            loc - scale * norm.ppf(alpha / 2)
 
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
+# def solution(p: float, x: np.array) -> tuple:
+#     # Измените код этой функции
+#     alpha = 1 - p
+#     n = len(x)
+#     loc = np.mean(x) / 44
+#     scale = np.sqrt(np.var(x) / n) / 44
+#     t_value = t.ppf(1 - alpha / 2, n - 1)
+#     left_bound = loc - t_value * scale
+#     right_bound = loc + t_value * scale
+#     return left_bound, right_bound
+
+def solution(p, x):
     alpha = 1 - p
-    n = len(x)
-    loc = np.mean(x) / 44
-    scale = np.sqrt(np.var(x) / n) / 44
-    t_value = t.ppf(1 - alpha / 2, n - 1)
-    left_bound = loc - t_value * scale
-    right_bound = loc + t_value * scale
-    return left_bound, right_bound
+    loc = (x.mean() - 0.5) / 968.0
+    scale = 1 / (968.0 * len(x))
+    return gamma.ppf(alpha / 2, len(x), loc=loc, scale=scale), \
+           gamma.ppf(1 - alpha / 2, len(x), loc=loc, scale=scale)
